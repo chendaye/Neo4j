fec6253a2a51fa10e8a361da8dccd333de27ac9f
chendaye <chendaye666@gmail.com>
2020-05-31 00:24:02 +0800

    neo4j

---
+++ src/test/java/top/chendaye666/embedded/Neo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chendaye666.embedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
