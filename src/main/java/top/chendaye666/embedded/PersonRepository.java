e238f1ab630039dfb4de690f602b2fe0dadf94ec
chendaye666 <chendaye666@gmail.com>
2020-06-10 01:11:55 +0800

    neo4j

---
+++ src/main/java/top/chendaye666/embedded/PersonRepository.java
@@ -0,0 +1,8 @@
+package top.chendaye666.embedded;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    Person findByName(String name);
+}
